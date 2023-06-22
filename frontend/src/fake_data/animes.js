13f8492d8b01b372d9ee4f756c3979a99f39543a
Ilya <Bezzubik12345@i.ua>
2023-06-22 19:23:49 +0300

    change in fake data

--- frontend/src/fake_data/animes.js
+++ frontend/src/fake_data/animes.js
@@ -1,7 +1,7 @@
 export const animes = [
     {
         id: 1,
-        name: "anime1",
+        name: "anime1 with big big big bigbig big big big big big name",
         description: "top anime: really top and good and cool and so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on so on",
         image_url: "https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg"
     },

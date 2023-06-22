cae5ee194bfe6a9132c052253c24781d4621d8d1
Ilya <Bezzubik12345@i.ua>
2023-06-22 19:23:38 +0300

    add description to animesmall; prettify this component

--- frontend/src/components/anime_small/AnimeSmall.js
+++ frontend/src/components/anime_small/AnimeSmall.js
@@ -2,13 +2,26 @@ import './AnimeSmall.css'
 
 export const AnimeSmall = (props) => {
     const { name, description, image_url } = props
-    const descriptionPrettified = description[0].toUpperCase() + description.slice(1)
+
+    const textPrettification = (text, max_length) => {
+        text = text[0].toUpperCase() + text.slice(1)
+        if (text.length > max_length) {
+            text = text.slice(0, max_length) + '...'
+        }
+        return text
+    }
+
+    const prettifiedDescription = textPrettification(description, 125)
+    const prettifiedName = textPrettification(name, 30)
     const backgroundImage = 'url(' + image_url + ')'
 
     return (
         <div className='anime_component'>
             <div className='anime_component_description'>
-                <p> {descriptionPrettified} </p>
+                <p> {prettifiedDescription} </p>
+            </div>
+            <div className='anime_component_name'>
+                <p> {prettifiedName} </p>
             </div>
             <div className='anime_component_background' style={{ backgroundImage: backgroundImage }} />
         </div>

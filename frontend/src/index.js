769085708d6f76525b79c869c7c5781de5f76157
Ilya <Bezzubik12345@i.ua>
2023-06-23 16:44:26 +0300

    bug fix + new component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './components/home/Home'
 import { Login } from './components/login/Login'
+import { AnimePage } from './components/anime_page/AnimePage'
 import './index.css'
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path='/' Component={Home} />
         <Route path='/login' Component={Login} />
+        <Route path='/anime/:animeId' Component={AnimePage} />
       </Routes>
     </BrowserRouter>
   )

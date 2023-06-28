c988c09a1080b6e10e8f1cf74d8cc2580e8b92a7
Ilya <Bezzubik12345@i.ua>
2023-06-28 12:46:12 +0300

    first login page prototype; without submit button, so without logic

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,4 +1,38 @@
+import { Link } from "react-router-dom"
+import "./Login.css"
+import { useState } from "react"
+
 const Login = () => {
+    const [formType, setFormType] = useState("login")
+
+    return (
+        <div className="login_page">
+            <form className="login_form">
+                <div className="type_buttons">
+                    <button className={formType == "login" ? "selected_type" : ""}
+                        onClick={(event) => {
+                            event.preventDefault()
+                            setFormType("login")
+                        }}>Вход</button>
+
+                    <button className={formType == "registration" ? "selected_type" : ""}
+                        onClick={(event) => {
+                            event.preventDefault()
+                            setFormType("registration")
+                        }}>Регистрация</button>
+                </div>
+                
+                <div className="form_inputs">
+                    <input placeholder="Имя пользователя" />
+                    <input placeholder="Пароль" />
+                    {formType == "registration" &&
+                        <input placeholder="Почта" style={{animation:"backInRight 0.75s linear"}} />
+                    }
+                </div>
+                <i class="gg-arrow-right-r"></i>
+            </form>
+        </div>
+    )
 
 }
 

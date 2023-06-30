40af5a9900f034f180158555108a44f1904d4413
Ilya <Bezzubik12345@i.ua>
2023-06-30 18:01:17 +0300

    add submit button to login page

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,12 +1,12 @@
 import "./Login.css"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 const Login = () => {
     const [formType, setFormType] = useState("login")
 
     return (
         <div className="login_page">
-            <form className="login_form" onSubmit={() => console.log(1)}>
+            <form className="login_form" onSubmit={(event) => event.preventDefault()}>
                 <div className="type_buttons">
                     <button className={formType == "login" ? "selected_type" : ""}
                         onClick={(event) => {
@@ -20,14 +20,20 @@ const Login = () => {
                             setFormType("registration")
                         }}>Регистрация</button>
                 </div>
-                
+
                 <div className="form_inputs">
                     <input placeholder="Имя пользователя" />
                     <input placeholder="Пароль" />
                     {formType == "registration" &&
-                        <input placeholder="Почта" style={{animation:"backInRight 0.75s linear"}} />
+                        <input placeholder="Почта" style={{ animation: "backInRight 0.75s linear" }} />
                     }
                 </div>
+
+                {formType == "login" &&
+                    <button className="login_button">Вход</button>}
+                {formType == "registration" &&
+                    <button className="registration_button">Регистрация</button>}
+
             </form>
         </div>
     )

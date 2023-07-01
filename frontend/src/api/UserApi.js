6f64616520a986046cc6d31890301ee48824be3a
Ilya <Bezzubik12345@i.ua>
2023-07-01 12:26:02 +0300

    new method in userapi to get info of user by token

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -2,6 +2,12 @@ import axios from 'axios'
 import responseTypeEnum from '../enums/ResponseTypeEnum.js'
 import * as constants from './Constants.js'
 
+const config = {
+    headers: {
+        "Content-Type": "multipart/form-data"
+    }
+}
+
 export default class UserApi {
 
     static createUser = (username, password) => {
@@ -10,7 +16,7 @@ export default class UserApi {
             username: username,
             password: password
         }
-        const responseData = axios.post(requestedUrl, data)
+        const responseData = axios.post(requestedUrl, data, config)
             .then((response) => {
                 return response.data
             })
@@ -26,10 +32,20 @@ export default class UserApi {
             username: username,
             password: password
         }
-        const config = {
-            headers: {
-                "Content-Type": "multipart/form-data"
-            }
+        const responseData = axios.post(requestedUrl, data, config)
+            .then((response) => {
+                return response.data
+            })
+            .catch((error) => {
+                return responseTypeEnum.error
+            })
+        return responseData
+    }
+
+    static getUserInfoByToken = (token) => {
+        const requestedUrl = constants.backendUrl + "get_user_info_by_token"
+        const data = {
+            key: token
         }
         const responseData = axios.post(requestedUrl, data, config)
             .then((response) => {

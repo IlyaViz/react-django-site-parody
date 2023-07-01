9a1214c6a032be0aa9868ab6cfa9d3b3eca28fd7
Ilya <Bezzubik12345@i.ua>
2023-07-01 16:38:38 +0300

    add function to convert string with underscores to camel case; add object prettification and array of objects prettification functions using previously created function

--- frontend/src/utils/prettify.js
+++ frontend/src/utils/prettify.js
@@ -5,3 +5,42 @@ export const textPrettification = (text, max_length) => {
     }
     return text
 }
+
+export const toCamelCase = (key) => {
+    let indexToReplace = []
+    for (let index = 0; index < key.length; index++) {
+        if (key[index] == "_") {
+            indexToReplace.push(index - indexToReplace.length)
+        }
+    }
+
+    let newKey = key.replaceAll("_", "")
+    indexToReplace.forEach((underscoreIndex) => {
+        const leftPart = newKey.substring(0, underscoreIndex)
+        const rightPart = newKey.substring(underscoreIndex + 1)
+        newKey = leftPart + newKey[underscoreIndex].toUpperCase() + rightPart
+    })
+    return newKey
+}
+
+export const jsonObjectPrettification = (jsonObject) => {
+
+    const keys = Object.keys(jsonObject)
+    const values = Object.values(jsonObject)
+    let newJsonObject = {}
+    keys.forEach((key, index) => {
+        const newKey = toCamelCase(key)
+        newJsonObject[newKey] = values[index]
+    })
+
+    return newJsonObject;
+}
+
+export const jsonObjectArrayPrettification = (array) => {
+    let newArray = []
+    array.forEach((jsonObject) => {
+        const newJsonObject = jsonObjectPrettification(jsonObject)
+        newArray.push(newJsonObject)
+    })
+    return newArray
+}

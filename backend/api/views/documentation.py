7e727bd80b768e4efe6e4a9314abc2d30766f6ff
Ilya <Bezzubik12345@i.ua>
2023-07-03 15:46:05 +0300

    rewrite documentation in prettier way

--- backend/api/views/documentation.py
+++ backend/api/views/documentation.py
@@ -1,44 +1,41 @@
-from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ..serializers import EpisodeSerializer
 
-DOCUMENTATION = {
-    "/create_user":{
-        "methods": ["post"],
-        "parameters":["username", "password"],
-        "description": "if user with this credential does not exist, then creates user. Otherwise returns error"
-    },
-    "/get_token":{
-        "methods": ["post"],
-        "parameters":["username", "password"],
-        "description": "if user with this credentials exist, then create or retrieve api token. Otherwise returns error"
-    },
-    "create_anime/":{
-        "methods": ["post"],
-        "parameters":["name", "description", "image"],
-        "description": "name should be unique"
-    },
-    "get_anime/<int:pk>/":{
-        "methods": ["get"],
-        "parameters":[],
-        "description":"get anime by id <pk>"
-    },
-    "add_episode/":{
-        "methods": ["post"],
-        "parameters":["name", "episode number", "episode", "anime"],
-        "description":"name should be unique, episode is videofile, anime is id of anime"
-    },
-    "get_episodes/<int:anime_pk>":{
-        "methods":["get"],
-        "parameters":[],
-        "description":"get episodes for anime with id <anime_pk>"
+def get_prettified_endpoint_documentation(endpoint:str, 
+                          methods:list, 
+                          post_data:list, 
+                          description:str) -> dict:
+    result = {
+        "endpoint":endpoint,
+        "methods":methods,
+        "post data":post_data,
+        "description":description
     }
-}
+    return result
 
-class Documentation(ListAPIView):
+class Documentation(APIView):
     def get(self, request, *args, **kwargs):
-        return Response(DOCUMENTATION)
-    
+        result = []
+        data = [
+            ["create_user", ["post"], ["username", "password"], "create user if not already exists"],
+            ["get_token", ["post"], ["username", "password"], "get or create token if user exists"],
+            ["validate_token", ["get"], [], "validate token recieved in Authorization header"],
+            ["get_user_info", ["get"], [], "get user info by token recieved in Authorization header"],
+            ["append_user_anime_watch_history", ["post"], ["anime_id"], "append anime taking by anime_id to history of User found by token recieved in Authorization header"],
+            ["get_user_anime_watch_history", ["get"], [], "get anime watch history of User found by token recieved in Authorization header"],
+            ["add_user_favourite_anime", ["post"], ["anime_id"], "add favourite anime to User found by token recieved in Authorization header"],
+            ["get_user_favourite_animes", ["get"], [], "get favourite animes of User found by token recieved in Authorization header"],
+            ["create_anime", ["post"], ["name", "description", "image"], "create new anime; image should be a file"],
+            ["get_anime/<int:pk>", ["get"], [], "return anime by it's id = pk"],
+            ["get_new_animes/<int:count>", ["get"], [], "return <count> new animes"],
+            # bug with that endpoint ["add_episode", ["post"], ["name", "episode_number", "anime"]],
+            ["get_episodes/<int:anime_pk>", ["get"], [], "return episodes for anime with id = anime_pk"],
+            ["get_episode_video/<int:episode_pk>", ["get"], [], "return episode with id = episode_pk with rewind feature"]
+        ]
+        for instance in data:
+            documentation_dict = get_prettified_endpoint_documentation(*instance)
+            result.append(documentation_dict)
+        return Response(result)
+        
 class TestAPIView(APIView):
     pass

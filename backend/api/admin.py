900b62f816cdb9b42d0c71fffac97edc9eb3688b
Ilya <Bezzubik12345@i.ua>
2023-07-03 14:13:18 +0300

    add favourite anime model to admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Anime, Episode, AnimeWatchHistory
+from .models import (Anime, 
+                     Episode, 
+                     AnimeWatchHistory,
+                     FavouriteAnime)
 
 # Register your models here.
 admin.site.register(Anime)
 admin.site.register(Episode)
 admin.site.register(AnimeWatchHistory)
+admin.site.register(FavouriteAnime)

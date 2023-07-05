c301736ca88f20ba1fb52589a5e248de02d41941
Ilya <Bezzubik12345@i.ua>
2023-07-05 16:58:14 +0300

    my page first prototype

--- frontend/src/components/my_page/MyPage.js
+++ frontend/src/components/my_page/MyPage.js
@@ -1,6 +1,87 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Nav from '../nav/Nav'
+import AnimeCard from '../anime_card/AnimeCard'
+import animeCardTypeEnum from '../../enums/AnimeCardTypeEnum'
+import UserApi from '../../api/UserApi.js'
 import './MyPage.css'
+import responseTypeEnum from '../../enums/ResponseTypeEnum.js'
 
 const MyPage = () => {
+
+    const [selectedButton, setSelectedButton] = useState("Последние просмотры")
+    const [animeList, setAnimeList] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        UserApi.validateLocalStorageToken().then((res) => {
+            if (res == responseTypeEnum.error) {
+                navigate("/login")
+            }
+        })
+    }, [localStorage])
+
+
+    useEffect(() => {
+        switch (selectedButton) {
+            case "Последние просмотры":
+                UserApi.getUserAnimeWatchHistory().then((res) => {
+                    if (res != responseTypeEnum.error) {
+                        setAnimeList(res)
+                    }
+                })
+                break;
+
+            case "Любимые":
+                UserApi.getUserFavouriteAnimes().then((res) => {
+                    if (res != responseTypeEnum.error) {
+                        console.log(res)
+                        setAnimeList(res)
+                    }
+                })
+                break;
+        }
+
+
+    }, [selectedButton])
+
+    return (
+        <div className='my_page'>
+            <Nav />
+            <div className='activity_type_buttons'>
+
+                <button
+                    className={selectedButton == "Последние просмотры" ? "selected_button" : ""}
+                    onClick={() => { setSelectedButton("Последние просмотры") }}>
+
+                    {window.innerWidth > 1200 ?
+                        <p>Последние просмотры</p>
+                        :
+                        <p>Смотрели</p>
+                    }
+
+                </button>
+
+                <button
+                    className={selectedButton == "Любимые" ? "selected_button" : ""}
+                    onClick={() => { setSelectedButton("Любимые") }}>
+
+                    Любимые
+                </button>
+
+            </div>
+
+            <div className='my_animes'>
+                {animeList.map((anime) => {
+                    return <AnimeCard
+                        type={animeCardTypeEnum.small}
+                        animeObject={anime}
+                    />
+                })}
+
+            </div>
+        </div >
+    )
 
 }
 

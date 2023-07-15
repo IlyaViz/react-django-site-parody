dd23560c0a618a1735b64e07218af5281a16ee10
Ilya <Bezzubik12345@i.ua>
2023-07-15 18:25:08 +0300

    rename bad named methods that handle model signals

--- backend/api/models.py
+++ backend/api/models.py
@@ -43,14 +43,14 @@ class Comment(models.Model):
 
 # delete episode when Episode is deleted
 @receiver(models.signals.post_delete, sender=Episode)
-def auto_delete_file_on_change(sender, instance, **kwargs):
+def auto_delete_file_on_episode_delete(sender, instance, **kwargs):
     if instance.episode:
         if os.path.isfile(instance.episode.path):
             os.remove(instance.episode.path)   
 
 # delete image when Anime is deleted
 @receiver(models.signals.post_delete, sender=Anime)
-def auto_delete_file_on_change(sender, instance, **kwargs):
+def auto_delete_file_on_anime_delete(sender, instance, **kwargs):
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)   

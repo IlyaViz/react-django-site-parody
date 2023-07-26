20d4337823b6cf602896cb9469becd197dca212f
Ilya <Bezzubik12345@i.ua>
2023-07-26 11:39:53 +0300

    adjusting bot for pagination

--- telegram-bot/api.py
+++ telegram-bot/api.py
@@ -3,19 +3,26 @@ from constants import BACKEND_URL, HEADERS
 
 def get_anime_ids_of_new_episodes():
     requested_url = BACKEND_URL + "get_new_episodes"
-    response = requests.get(requested_url).json()
+    response = requests.get(requested_url)
+    if response.status_code != 200:
+        raise ValueError("No page found")
+    response = response.json()
     result = []
     for episode in response:
         result.append(episode["anime"])
     return result
 
-def get_anime_distribution_subscribers_telegram_chat_ids(anime_id):
-    requested_url = BACKEND_URL + "get_anime_distribution_subscribers_telegram_chat_ids/" + f'{anime_id}'
-    response = requests.get(requested_url, headers=HEADERS).json()
-    return response
+def get_telegram_chat_ids_of_anime_distribution_subscribers(anime_id, page):
+    requested_url = BACKEND_URL + "get_telegram_chat_ids_of_anime_distribution_subscribers/" + f'{anime_id}'
+    params = {"page": page}
+    response = requests.get(requested_url, params=params, headers=HEADERS)
+    if response.status_code != 200:
+        raise ValueError("No page found")
+    return response.json()
 
 def get_anime_info(anime_id):
     requested_url = BACKEND_URL + "get_anime/" + f'{anime_id}'
-    response = requests.get(requested_url).json()
-    return response
-
+    response = requests.get(requested_url)
+    if response.status_code != 200:
+        raise ValueError("No page found")
+    return response.json()
--- telegram-bot/distribution.py
+++ telegram-bot/distribution.py
@@ -1,7 +1,7 @@
 from telegram import Bot, ParseMode
 from constants import TOKEN
 from api import (get_anime_ids_of_new_episodes, 
-                 get_anime_distribution_subscribers_telegram_chat_ids,
+                 get_telegram_chat_ids_of_anime_distribution_subscribers,
                  get_anime_info)
 from utils import get_list_difference
 import time
@@ -16,12 +16,18 @@ def episode_distribution():
     for anime_id in difference:
         print(f"sending telegram messages for anime with id {anime_id}")
         anime_name = get_anime_info(anime_id)["name"]
-        chat_ids = get_anime_distribution_subscribers_telegram_chat_ids(anime_id)
-        for chat_id in chat_ids:
+        page = 1
+        while True:
             try:
-                bot.send_message(chat_id=chat_id, text=f"{anime_name}: *только что вышла новая серия*", parse_mode=ParseMode.MARKDOWN)
+                chat_ids = get_telegram_chat_ids_of_anime_distribution_subscribers(anime_id, page)
+                page += 1
             except:
-                pass
+                break
+            for chat_id in chat_ids:
+                try:
+                    bot.send_message(chat_id=chat_id, text=f"{anime_name}: *только что вышла новая серия*", parse_mode=ParseMode.MARKDOWN)
+                except:
+                    pass
     current_anime_ids = anime_ids
 
 if __name__ == '__main__':

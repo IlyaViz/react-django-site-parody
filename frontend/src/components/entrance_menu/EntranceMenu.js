6c43257ce1aef0a18d2c29b73a6cc65233d16d9f
Ilya <Bezzubik12345@i.ua>
2023-08-05 16:25:22 +0300

    entrance menu code prettification

--- frontend/src/components/entrance_menu/EntranceMenu.js
+++ frontend/src/components/entrance_menu/EntranceMenu.js
@@ -1,9 +1,9 @@
 import './EntranceMenu.css'
 
-const EntranceMenu = (props) => {
+const EntranceMenu = () => {
 
     return (
-        <div>
+        <div className='entrance_menu'>
             <div className="entrance_text">
                 <h1 className="entrance_main_text animate__animated animate__bounceInUp">Смотри аниме на пародии <span>AniType</span></h1>
                 <h4 className="entrance_extra_text animate__animated animate__bounceInUp">Огромное количество релизов с озвучкой от любимых студий, не очень современный дизайн и не только, зарегистрируйся прямо сейчас</h4>

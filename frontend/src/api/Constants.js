4a5949b53e4277eb023c07a0eb0563c91d02d854
Ilya <Bezzubik12345@i.ua>
2023-08-14 16:45:06 +0300

    optimize docker-compose and files(k8s) for frontend in production

--- frontend/src/api/Constants.js
+++ frontend/src/api/Constants.js
@@ -1,6 +1,6 @@
 // in development will be used REACT_APP_BACKEND_IP, in production second ip;
 // the second ip must be replaced with server ip
-const BACKEND_IP = process.env.REACT_APP_BACKEND_IP ?? "127.0.0.1:8000"
+const BACKEND_IP = process.env.REACT_APP_BACKEND_IP ?? "127.0.0.1:30800"
 
 export const backendUrl = `http://${BACKEND_IP}/api/`
 export const config = {

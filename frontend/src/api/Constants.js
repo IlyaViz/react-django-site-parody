159634a048d27a4d6122c158448cffcaf77e3887
Ilya <Bezzubik12345@i.ua>
2023-08-14 16:02:36 +0300

    add production dockerfile version

--- frontend/src/api/Constants.js
+++ frontend/src/api/Constants.js
@@ -1,3 +1,5 @@
+// in development will be used REACT_APP_BACKEND_IP, in production second ip;
+// the second ip must be replaced with server ip
 const BACKEND_IP = process.env.REACT_APP_BACKEND_IP ?? "127.0.0.1:8000"
 
 export const backendUrl = `http://${BACKEND_IP}/api/`
